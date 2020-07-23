903e47ec9f3fd7d5feb9938a01140d557c8ba7d1
Stephen Rodrigues <stephen.rods21@gmail.com>
2020-07-24 05:29:31 +0700

    staticfiles addeds5

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'schooldio.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'schooldio-D.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
